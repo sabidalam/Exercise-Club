c4ec77d42adef0d68f4268979186e15d8a38a7e3
sabidalam <Shafkatsaad@gmail.com>
2022-09-29 13:16:04 +0600

    displayed all info in user-ui

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './User.css';
-import logo from '../../images/me.jpg'
+import logo from '../../images/me.jpg';
 
 const User = () => {
     return (
@@ -11,9 +11,43 @@ const User = () => {
                     <h3>Shafkat Saad</h3>
                     <span>Dhaka, Bangladesh</span>
                 </div>
-
             </div>
-
+            <div className='user-bio'>
+                <div>
+                    <h3>65kg</h3>
+                    <span>Weight</span>
+                </div>
+                <div>
+                    <h3>5.8</h3>
+                    <span>Height</span>
+                </div>
+                <div>
+                    <h3>23yrs</h3>
+                    <span>Age</span>
+                </div>
+            </div>
+            <div className='break'>
+                <h3>Add A Break</h3>
+                <div className='break-time'>
+                    <button className='time-btn'>20s</button>
+                    <button className='time-btn'>30s</button>
+                    <button className='time-btn'>40s</button>
+                    <button className='time-btn'>50s</button>
+                    <button className='time-btn'>60s</button>
+                </div>
+            </div>
+            <div className='exercise-details'>
+                <h3>Exercise Details</h3>
+                <div className='exercise-time'>
+                    <h4>Exercise Time</h4>
+                    <span>seconds</span>
+                </div>
+                <div className='break-time'>
+                    <h4>Break time</h4>
+                    <span>seconds</span>
+                </div>
+            </div>
+            <button className='complete-btn'>Activity Completed</button>
         </div>
     );
 };
